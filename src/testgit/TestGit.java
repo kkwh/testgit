6054933447761c6f2023a6c32ba2780f8a801742
ITWILL <ITWILL@DESKTOP-SP214SU>
2023-05-23 20:04:29 +0900

    second

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -2,7 +2,7 @@ package testgit;
 
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("git test");
+        System.out.println("git test");lll
        
     }
 }
